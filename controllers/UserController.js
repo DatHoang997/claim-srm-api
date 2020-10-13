16f8f0c2f2dd99213d61838fdc0053b06f863534
dathoang <dathoang997@gmail.com>
2020-10-13 16:06:32 +0700

    swap srm

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,6 @@
 const User = require("../models/UserModel")
-const { body, validationResult } = require("express-validator")
+const ClaimSrm = require("../models/ClaimSrmModel")
+const { body, validationResult, Result } = require("express-validator")
 const apiResponse  = require("../helpers/apiResponse")
 const auth = require("../middlewares/jwt")
 const mongoose = require("mongoose")
@@ -10,10 +11,64 @@ const Utils = require("../helpers/utils")
 const {web3ws, web3eth} = require("../helpers/web3")
 const firebase = require('../helpers/firebase')
 const axios = require("axios")
+const { setQueues } = require('bull-board')
+var Queue = require('bull');
+const bigDecimal = require('js-big-decimal');
+const {
+  weiToPOC,
+  srmToWei
+} = require('../helpers/utils')
 
 mongoose.set("useFindAndModify", false)
 
-exports.claimZSRM = [
+const queues = new Queue('queue', {redis: {port: process.env.REDIS_PORT, host: '127.0.0.1'}});
+// console.log(queues)
+const connection = new Connection('http://testnet.solana.com', 'recent');
+  queues.process(async function(job, done) {
+    // done()
+    console.log(job.data)
+    let claimSrm = await ClaimSrm.findOne({tx_hast : job.data.txHash})
+    console.log(claimSrm)
+    if (claimSrm != null) {
+      console.log('falseeeeeeeeeee')
+    }
+    let confirm = await web3ws.eth.getTransaction(job.data.txHash)
+    console.log('transaction', confirm)
+    let amount = weiToPOC(await web3ws.utils.hexToNumberString('0x' + confirm.input.slice(74)))
+    console.log(amount, await web3ws.utils.hexToNumberString('0x' + confirm.input.slice(74)))
+    let wallet = await web3ws.eth.accounts.recover(job.data.message, job.data.signature).toLowerCase()
+    let srmAddress = job.data.message.slice(job.data.message.indexOf('.') + 1, job.data.message.lastIndexOf('.'))
+    console.log('srmAddress',srmAddress)
+    let result = srmToWei(bigDecimal.multiply(amount, 1000))
+    console.log('result', result)
+    console.log(confirm.from.toLowerCase() == wallet,confirm.from , wallet)
+    if (confirm.to == process.env.POC_CONTRACT_ADDRESS && confirm.from.toLowerCase() == wallet) {
+      let { address, publicKey, account, privateKey } = await Utils.getSolanaAccountAtIndex(process.env.MNEMONIC)
+      let recentBlockhash = await connection.getRecentBlockhash('recent')
+      let transaction = new Transaction({recentBlockhash: recentBlockhash.blockhash})
+      .add(
+        slnUtils.transfer({
+          owner: account.publicKey, // from SOL address
+          source: new PublicKey(process.env.SRM_ADDRESS), // from SRM address
+          destination: new PublicKey(srmAddress), // to SRM address
+          amount: result //ok
+        })
+      )
+      console.log(transaction)
+      connection.sendTransaction(transaction, [account]).then(transfer=>{
+        job.progress(100)
+        done()
+        console.log('transfer',transfer)
+      }).catch(error=>{
+        console.log("error",error)
+        throw new Error(error)
+      })
+    }
+  })
+
+  setQueues(queues)
+
+exports.claimASRM = [
   body("data", "data can not be empty.").not().isEmpty().trim(),
   body("signature", "signature can not be empty.").not().isEmpty().trim(),
   async function (req, res) {
@@ -24,27 +79,27 @@ exports.claimZSRM = [
     console.log(req.body)
     let fbId = req.body.data.slice(0, req.body.data.indexOf('.'))
     console.log('fb_id', fbId)
-    let zsrmAddress = req.body.data.slice(req.body.data.indexOf('.') + 1, req.body.data.lastIndexOf('.'))
-    console.log('zsrm', zsrmAddress)
+    let asrmAddress = req.body.data.slice(req.body.data.indexOf('.') + 1, req.body.data.lastIndexOf('.'))
+    console.log('asrm', asrmAddress)
     let user = await User.findOne({fb_id: fbId})
     console.log(user)
     // if (user.claimed == '1') { //disable when testing
     //   return apiResponse.successResponse(res, "already claimed") //disable when testing
     // } //disable when testing
-    // let check = await User.findOne({wallet_address: zsrmAddress}) //disable when testing
+    // let check = await User.findOne({wallet_address: asrmAddress}) //disable when testing
     // if (check != null) { //disable when testing
     //   return apiResponse.successResponse(res, "already claimed") //disable when testing
     // } //disable when testing
     const wallet = await web3ws.eth.accounts.wallet.add(process.env.POC_PRIVATE_KEY);
     const pocBalance = await global.token_contract.methods.balanceOf(wallet.address).call({from: wallet.address, gasPrice: '0'})
-    // if (parseFloat(pocBalance) - process.env.SRM_REWARD < 500000000000000000000) {
+    // if (parseFloat(pocBalance) - process.env.ASRM_REWARD < 500000000000000000000) {
     //   // mailer.send('noreply@pocvietnam.com', 'im@loc.com.vn', "Pool's Warning!", "POC pool's balance is below 500")
     //   // mailer.send('noreply@pocvietnam.com', 'daohoangthanh@gmail.com', "Pool's Warning!", "POC Pool's balance is below 500")
     //   console.log('check balance')
     // }
-    if (parseFloat(pocBalance) > process.env.SRM_REWARD) {
+    if (parseFloat(pocBalance) > process.env.ASRM_REWARD) {
       try {
-        global.token_contract.methods.transfer(zsrmAddress, process.env.SRM_REWARD)
+        global.token_contract.methods.transfer(asrmAddress, process.env.ASRM_REWARD)
         .send({from: wallet.address, gasPrice: '0'}
         ).then(async function (data) {
           const lastCheck = async () => {
@@ -52,7 +107,7 @@ exports.claimZSRM = [
             const check = await web3ws.eth.getTransaction(data.transactionHash)
             if (check.blockHash){
               clearInterval(checkFunction)
-              await User.findOneAndUpdate({fb_id: fbId}, {$set:{wallet_address: zsrmAddress, claimed: 1}})
+              await User.findOneAndUpdate({fb_id: fbId}, {$set:{wallet_address: asrmAddress, claimed: 1}})
               return apiResponse.successResponse(res, "transfer success")
             }
           }
@@ -71,35 +126,24 @@ exports.claimZSRM = [
 
 // r8YtgnjtH3rh4Wj2VYGWawXBNgapSohvgMJETGwNYDR  receive
 exports.swapSRM = [
-  body("data", "data can not be empty.").not().isEmpty().trim(),
+  body("message", "data can not be empty.").not().isEmpty().trim(),
   body("signature", "signature can not be empty.").not().isEmpty().trim(),
+  body("txHash", "txHash can not be empty.").not().isEmpty().trim(),
   async function (req, res) {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       return apiResponse.validationErrorWithData(res, "Validation Error.", errors.array())
     }
-    const connection = new Connection('http://testnet.solana.com', 'recent');
 
-    let { address, publicKey, account, privateKey } = await Utils.getSolanaAccountAtIndex(process.env.MNEMONIC)
-    const recentBlockhash = await connection.getRecentBlockhash('recent')
-    let transaction = new Transaction({recentBlockhash: recentBlockhash.blockhash})
-    .add(
-      slnUtils.transfer({
-        owner: account.publicKey, // from SOL address
-        source: new PublicKey('3F4EXyNCLxxk5auyS1aN8rcMX6MGvTqdmEraN2CHtjec'), // from SRM address
-        destination: new PublicKey('6VTiGtHw67jJxnftBMbmnE5g8jsGJhYfXm55csfWmS5W'), // to SRM address
-        amount: 10 //ok
-      }),
-    )
-
-    console.log(transaction)
-    return connection.sendTransaction(transaction, [account]).then(transfer=>{
-      console.log(transfer)
-      return apiResponse.successResponseWithData(res, "transfer success", transfer)
-    }).catch(error=>{
-      console.log("error",error)
-      throw new Error(error)
-    })
+    const run = () => {
+      queues.add({
+        message: req.body.message,
+        signature: req.body.signature,
+        txHash: req.body.txHash,
+      }, {lifo: true} );
+      return apiResponse.successResponseWithData(res, "transfer success", req.body.amount)
+    }
+    setTimeout(run, 1000)
   }
 ];
 
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ var express = require("express")
 var refRoute = require('./ref')
 var app = express()
 
+const { UI } = require('bull-board')
 app.use("/", refRoute)
+app.use('/queues/',
+//  function () {
+  UI
+// }
+)
 
 module.exports = app;
-
