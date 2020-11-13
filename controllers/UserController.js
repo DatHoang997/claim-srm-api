8abc84ea90a6c746d6fed6db83a05217f6ec1d0b
dathoang <dathoang997@gmail.com>
2020-11-13 09:25:46 +0700

    get SRM balance

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -87,28 +87,28 @@ const connection = new Connection('https://solana-api.projectserum.com', 'recent
       console.log('result', result, confirm.to == process.env.ASRM_CONTRACT_ADDRESS, confirm.from.toLowerCase() == wallet)
       if (confirm.to == process.env.ASRM_CONTRACT_ADDRESS && confirm.from.toLowerCase() == wallet) {
         let { address, publicKey, account, privateKey } = await Utils.getSolanaAccountAtIndex(process.env.SRM_MNEMONIC)
-
-        // let accountInfo = await connection.getAccountInfo(new PublicKey(address))
-        // console.log(accountInfo, accountInfo.owner)
-        // let mint, amount
-        // if (!accountInfo) {
-        //   console.log('!accountInfo')
-        // }
-        // console.log(accountInfo.owner.toBase58())
-        // console.log('aloooo', new PublicKey(
-        //   'G5xnaQGf5HmXSGqCCoQHWQDREgqzXRneFQY6sYAzVWc6',
-        // ))
-        // if (accountInfo.owner.toBase58() == new PublicKey('G5xnaQGf5HmXSGqCCoQHWQDREgqzXRneFQY6sYAzVWc6',)) {
-        //   console.log('innnnnnnnnnn')
-        //   console.log(slnUtils.parseTokenAccountData(accountInfo.data))
-        //   const data = slnUtils.parseTokenAccountData(accountInfo.data)
-        //   mint = data.mint
-        //   amount = data.amount
-        //   console.log('mint', mint, 'balance', amount)
-        // }
-        // if (!mint) {
-        //   console.log('!mint')
-        // }
+        console.log('@@@@@')
+        let accountInfo = await connection.getAccountInfo(new PublicKey(address))
+        console.log('accountInfo', accountInfo, accountInfo.owner)
+        let mint, amount
+        if (!accountInfo) {
+          console.log('!accountInfo')
+        }
+        console.log(accountInfo.owner.toBase58())
+        console.log('aloooo', new PublicKey(
+          'G5xnaQGf5HmXSGqCCoQHWQDREgqzXRneFQY6sYAzVWc6',
+        ))
+        if (accountInfo.owner.toBase58() == new PublicKey('G5xnaQGf5HmXSGqCCoQHWQDREgqzXRneFQY6sYAzVWc6',)) {
+          console.log('innnnnnnnnnn')
+          console.log(slnUtils.parseTokenAccountData(accountInfo.data))
+          const data = slnUtils.parseTokenAccountData(accountInfo.data)
+          mint = data.mint
+          amount = data.amount
+          console.log('mint', mint, 'balance', amount)
+        }
+        if (!mint) {
+          console.log('!mint')
+        }
 
         let recentBlockhash = await connection.getRecentBlockhash('recent')
         console.log('recentBlockhash',recentBlockhash)
